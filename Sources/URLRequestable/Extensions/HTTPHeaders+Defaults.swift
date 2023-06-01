c4f876836262444e9b83d541018bffb4f696d6f2
Waqar Malik <wmalloc@users.noreply.github.com>
2023-05-31 21:35:50 -0700

    Moved More tests (#3) (#4)

---
+++ Sources/URLRequestable/Extensions/HTTPHeaders+Defaults.swift
@@ -0,0 +1,15 @@
+//
+//  HTTPHeaders+Defaults.swift
+//
+//
+//  Created by Waqar Malik on 1/15/23.
+//  Copyright © 2020 Waqar Malik All rights reserved.
+//
+
+import Foundation
+
+public extension HTTPHeaders {
+	static var defaultHeaders: HTTPHeaders {
+		HTTPHeaders(arrayLiteral: HTTPHeader.defaultUserAgent, .defaultAcceptEncoding, .defaultAcceptLanguage)
+	}
+}
