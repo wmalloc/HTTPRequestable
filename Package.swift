4b71d39f0a55caa083b88e0df2b210509e013d4c
Waqar Malik <wmalloc@users.noreply.github.com>
2024-05-01 19:37:08 -0700

    Unified the request builder (#33)
    * Updated the Package depencey and gitignore
    
    * Unified the protocols
    
    * Fixed the field settings
    
    * HTTPURLResponse conversion
    
    * Removed the localied macro

--- Package.swift
+++ Package.swift
@@ -8,23 +8,21 @@ let package = Package(
   defaultLocalization: "en",
   platforms: [.iOS(.v15), .tvOS(.v15), .macOS(.v11), .watchOS(.v8), .macCatalyst(.v15)],
   products: [
-    // Products define the executables and libraries a package produces, and make them visible to other packages.
     .library(name: "URLRequestable", targets: ["URLRequestable"]),
     .library(name: "MultipartForm", targets: ["MultipartForm"]),
   ],
   dependencies: [
-    // Dependencies declare other packages that this package depends on.
     .package(url: "https://github.com/apple/swift-http-types.git", from: "1.0.3"),
   ],
   targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages this package depends on.
     .target(name: "URLRequestable", dependencies: [
       .product(name: "HTTPTypes", package: "swift-http-types"),
-      .product(name: "HTTPTypesFoundation", package: "swift-http-types")]),
+      .product(name: "HTTPTypesFoundation", package: "swift-http-types")],
+            resources: [.process("Resources")]),
     .target(name: "MultipartForm", dependencies: ["URLRequestable",
                                                   .product(name: "HTTPTypes", package: "swift-http-types"),
-                                                  .product(name: "HTTPTypesFoundation", package: "swift-http-types")]),
+                                                  .product(name: "HTTPTypesFoundation", package: "swift-http-types")],
+            resources: [.process("Resources")]),
     .testTarget(name: "URLRequestableTests",
                 dependencies: ["URLRequestable", "MultipartForm",
                                .product(name: "HTTPTypes", package: "swift-http-types"),
--- Sources/URLRequestable/Extensions/URLRequest+HTTPFields.swift
+++ Sources/URLRequestable/Extensions/URLRequest+HTTPFields.swift
@@ -9,58 +9,60 @@ import Foundation
 import HTTPTypes
 
 public extension URLRequest {
-	@discardableResult
-	func setHttpHeaderFields(_ fields: HTTPFields?) -> Self {
-		var request = self
-		request.headerFields = fields
-		return request
-	}
+  @discardableResult
+  func setHttpHeaderFields(_ fields: HTTPFields?) -> Self {
+    var request = self
+    request.headerFields = fields
+    return request
+  }
 
-	@discardableResult
-	func addHeaderFields(_ fields: HTTPFields?) -> Self {
-		guard let fields else {
-			return self
-		}
-		var request = self
-		for header in fields {
-			request.addValue(header.value, forHTTPHeaderField: header.name)
-		}
-		return request
-	}
+  @discardableResult
+  func addHeaderFields(_ fields: HTTPFields) -> Self {
+    var request = self
+    for header in fields {
+      request.addValue(header.value, forHTTPField: header.name)
+    }
+    return request
+  }
 }
 
 public extension URLRequest {
-	var headerFields: HTTPFields? {
-		get {
-			guard let allHTTPHeaderFields else {
-				return nil
-			}
-			return HTTPFields(rawValue: allHTTPHeaderFields)
-		}
-		set {
-			allHTTPHeaderFields = newValue?.rawValue
-		}
-	}
+  var headerFields: HTTPFields? {
+    get {
+      guard let allHTTPHeaderFields else {
+        return nil
+      }
+      return HTTPFields(rawValue: allHTTPHeaderFields)
+    }
+    set {
+      allHTTPHeaderFields = newValue?.rawValue
+    }
+  }
 }
 
 extension HTTPFields: RawRepresentable {
-	public typealias RawValue = [String: String]
+  public typealias RawValue = [String: String]
 
-	public init?(rawValue: [String: String]) {
-		self.init()
-		for (key, value) in rawValue {
-			guard let name = HTTPField.Name(key) else {
-				continue
-			}
-			self[name] = value
-		}
-	}
+  public init?(rawValue: [String: String]) {
+    self.init()
+    for (key, value) in rawValue {
+      guard let name = HTTPField.Name(key) else {
+        continue
+      }
+      self[name] = value
+    }
+  }
 
-	public var rawValue: [String: String] {
-		var rawValues: [String: String] = [:]
-		for value in self {
-			rawValues[value.name.rawName] = value.value
-		}
-		return rawValues
-	}
+  public var rawValue: [String: String] {
+    var rawValues: [String: String] = [:]
+    for field in self {
+      if let existingValue = rawValues[field.name.rawName] {
+        let separator = field.name == .cookie ? "; " : ", "
+        rawValues[field.name.rawName] = "\(existingValue)\(separator)\(field.value)"
+      } else {
+        rawValues[field.name.rawName] = field.value
+      }
+    }
+    return rawValues
+  }
 }
