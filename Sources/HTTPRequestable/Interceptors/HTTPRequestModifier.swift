1ee24b486fe3fb9294754ccb21b43075970294f2
Waqar Malik <wmalloc@users.noreply.github.com>
2025-08-24 10:13:13 -0700

    Added error for loggin, and urlrequest modifier (#100)

--- Sources/HTTPRequestable/Interceptors/HTTPRequestModifier.swift
+++ Sources/HTTPRequestable/Interceptors/HTTPRequestModifier.swift
@@ -14,7 +14,7 @@ public protocol HTTPRequestModifier: Sendable {
   /// This method allows for customization of the HTTP request before it is sent to the server. It provides an opportunity to modify the request properties, such as headers or body data, before the request is sent through a `URLSession`.
   ///
   /// - Parameters:
-  ///   - request: A reference to an `HTTPRequest` object that can be modified in place. The type of `HTTPRequest` must conform to `HTTPRequestable`.
+  ///   - request: A reference to an `HTTPRequest` object that can be modified in place.
   ///   - session: The `URLSession` through which the request will be sent. This parameter is provided so that you can adjust the request based on the session's configuration or other properties.
   /// - Throws: An error of type `Error` if there is an issue with the interceptor logic, such as invalid input.
   ///
@@ -23,9 +23,26 @@ public protocol HTTPRequestModifier: Sendable {
   ///
   /// - SeeAlso: `HTTPRequestable`, `URLSession`
   func modify(_ request: inout HTTPRequest, for session: URLSession) async throws
+
+  /// Intercepts and customizes the HTTP request before it is sent through a URLSession.
+  ///
+  /// This method allows for customization of the HTTP request before it is sent to the server. It provides an opportunity to modify the request properties, such as headers or body data, before the request is sent through a `URLSession`.
+  ///
+  /// - Parameters:
+  ///   - request: A reference to an `URLRequest` object that can be modified in place.
+  ///   - session: The `URLSession` through which the request will be sent. This parameter is provided so that you can adjust the request based on the session's configuration or other properties.
+  /// - Throws: An error of type `Error` if there is an issue with the interceptor logic, such as invalid input.
+  ///
+  /// - Note: The `HTTPRequest` type must conform to `HTTPRequestable` for this method to work correctly.
+  /// - Note: This method is called automatically by the `object(for:)` and `data(for:)` methods to allow for request customization.
+  ///
+  /// - SeeAlso: `HTTPRequestable`, `URLSession`
+  func modify(_ request: inout URLRequest, for session: URLSession) async throws
 }
 
 /// Default implemenation
 public extension HTTPRequestModifier {
   func modify(_ request: inout HTTPRequest, for session: URLSession) async throws {}
+
+  func modify(_ request: inout URLRequest, for session: URLSession) async throws {}
 }
