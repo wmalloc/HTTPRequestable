7887e208b9b3a4fd958567832789a8998d7df742
Waqar Malik <wmalloc@users.noreply.github.com>
2026-03-06 21:33:25 -0800

    Updated project to ios 26 and added webview to view articles

--- Sample/Sample/ContentView.swift
+++ Sample/Sample/ContentView.swift
@@ -1,11 +1,11 @@
 //
 //  ContentView.swift
-//  Sample
 //
 //  Created by Waqar Malik on 9/7/24.
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
   @Environment(SampleViewModel.self) var viewModel: SampleViewModel
@@ -15,13 +15,24 @@ struct ContentView: View {
 
     NavigationStack {
       List(viewModel.filteredItems) { item in
-        ItemDetailView(item: item)
+        if let url = item.url {
+          NavigationLink {
+            WebView(url: url)
+              .navigationTitle(item.title)
+              .toolbarBackground(Color.red)
+          } label: {
+            ItemDetailView(item: item)
+          }
+        } else {
+          ItemDetailView(item: item)
+        }
       }
       .listStyle(.plain)
       .scrollIndicators(.hidden)
       .navigationTitle(Text("Hacker News"))
       .navigationBarTitleDisplayMode(.inline)
       .toolbar(.visible, for: .navigationBar)
+      .toolbarBackground(Color.red)
       .searchable(text: $model.searchText, placement: .automatic, prompt: Text("Filter by title"))
       .refreshable {
         viewModel.loadTopStories()
